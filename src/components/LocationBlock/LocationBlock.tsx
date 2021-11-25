36958bb59fc701d03a5259da49c5d4f9a0b37458
Taraskiiin <taraskravets95@gmail.com>
2021-11-25 17:16:38 +0200

    refactor(total): eslint refactoring

--- src/components/LocationBlock/LocationBlock.tsx
+++ src/components/LocationBlock/LocationBlock.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import React from 'react'
+import ReactMapGL from 'react-map-gl'
 
-// type LocationBlockType = {
-//   latitude: number;
-//   longitude: number;
-//   zoom: number;
-//   mapboxApiAccessToken: string;
-// };
-
-export default function LocationBlock() {
-  // const [viewport, setViewport] = useState({
+const LocationBlock: React.FC = () => {
   const viewport = {
     latitude: 49.842957,
     longitude: 24.031111,
     zoom: 10,
     width: '100vw',
     height: '100vh',
-  };
+  }
   return (
     <ReactMapGL
       {...viewport}
-      mapboxApiAccessToken={`pk.eyJ1IjoidGFyYXNraWlpbiIsImEiOiJja3Zyd2Y3ZGgxbnR4MndxaDdtMnd4czg2In0.6IZcJc33_zLJHZ3fwhKnFA`}
-    ></ReactMapGL>
-  );
+      mapboxApiAccessToken='pk.eyJ1IjoidGFyYXNraWlpbiIsImEiOiJja3Zyd2Y3ZGgxbnR4MndxaDdtMnd4czg2In0.6IZcJc33_zLJHZ3fwhKnFA'
+    />
+  )
 }
+
+export default LocationBlock
